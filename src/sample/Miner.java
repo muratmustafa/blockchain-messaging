54285048b5699feb832b5a3356b56265f5e2d500
Mustafa Murat <mustafamurat1907@gmail.com>
2021-06-07 10:04:32 +0300

    test

--- src/sample/Miner.java
+++ src/sample/Miner.java
@@ -21,7 +21,7 @@ public class Miner extends User {
 	}
 
 	void broadCastMessage(String m) throws IOException {		
-		Broadcast.broadcast(m, Network.availableInterfaces().get(0), port);
+		Broadcast.broadcast(m, InetAddress.getByName("10.150.19.8"), port);
 	}
 
 	@Override
